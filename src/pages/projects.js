c21947a060cf116de0c2ba45daa1cf6343f98933
Jimmy Dovener <jdovener16@gmail.com>
2022-09-30 14:41:00 +0100

    JD: oops fix

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -27,7 +27,7 @@ export default function Project() {
         <div>
         <br></br>
             <div>
-                <h1>QA Practical Project </h1>
+                <h1>Projects</h1>
             </div>
             <div>
                 <h3>1. Crimble Consulting</h3>
